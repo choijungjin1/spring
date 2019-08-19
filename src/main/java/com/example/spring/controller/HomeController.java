a77e7228ed85b582ba88dd41a67b761848bb6803
choijungjin1 <cjj1002@naver.com>
2019-08-19 18:02:44 +0900

    06:02

---
+++ src/main/java/com/example/spring/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/home")
+    public String home() {
+        return "index";
+    }
+}
