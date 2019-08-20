33e932db1c0753edaf665fcda6057743a22f30cf
choijungjin1 <cjj1002@naver.com>
2019-08-20 11:07:57 +0900

    11:07

--- src/main/java/com/example/spring/controller/MemberController.java
+++ src/main/java/com/example/spring/controller/MemberController.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
     @RequestMapping(value = "/member/list")
     public void memberAction() {
-
+    }
+    @RequestMapping(value = "/member/view")
+    public void memberview() {
+    }
+    @RequestMapping(value = "/member/delete")
+    public void memberdelete() {
+    }
+    @RequestMapping(value = "/member/edit")
+    public void memberedit() {
     }
 }
--- src/main/java/com/example/spring/controller/noticecontroller.java
+++ src/main/java/com/example/spring/controller/noticecontroller.java
@@ -5,19 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class noticecontroller {
-    @RequestMapping(value = "/member/view")
-    public void memberview() {
-    }
-    @RequestMapping(value = "/member/delete")
-    public void memberdelete() {
-    }
-    @RequestMapping(value = "/member/edit")
-    public void memberedit() {
-    }
     @RequestMapping(value = "/member/login")
     public void memberlogin() {
     }
     @RequestMapping(value = "/member/logout")
     public void memberlogout() {
+    }
+    @RequestMapping(value = "/member/signup")
+    public void membersignup() {
     }
 }
